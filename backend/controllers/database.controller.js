d882eb22ff234c8739f40a412b17ed53fde849e0
DmitriyHoff <goffdmitriy@gmail.com>
2023-08-08 01:44:54 +0200

    simplify database.controller code

--- backend/controllers/database.controller.js
+++ backend/controllers/database.controller.js
@@ -21,102 +21,89 @@ const slugParams = {
 function getRequestHostUrl(request) {
   return `${request.protocol}://${request.get("host")}/`;
 }
+
+async function sendResponse(response, callback) {
+  try {
+    const data = await callback();
+
+    // если в ответе есть ошибка, меняем статус
+    if (data?.error) response.statusCode = 400;
+
+    response.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 /* Получение списка категорий */
 router.route("/categories").get((request, response) => {
-  const result = database.getCategories();
-  result.then((data) => response.json(data)).catch((err) => console.log(err));
+  sendResponse(response, () => {
+    return database.getCategories();
+  });
 });
 
 /* Получение списка производителей */
 router.route("/brands").get((request, response) => {
-  const result = database.getBrands();
-  result.then((data) => response.json(data)).catch((err) => console.log(err));
+  sendResponse(response, () => {
+    return database.getBrands();
+  });
 });
 
 /* Получение списка товаров */
 router.route("/products").get((request, response) => {
   const hostname = getRequestHostUrl(request);
-  const result = database.getProducts(hostname);
-  result.then((data) => response.json(data)).catch((err) => console.log(err));
+
+  sendResponse(response, () => {
+    return database.getProducts(hostname);
+  });
 });
 
 /* Получение товара по id */
 router.route("/product").get((request, response) => {
   const hostname = getRequestHostUrl(request);
   const productId = request.query.id;
-  const result = database.getProductById(productId, hostname);
-  result
-    .then((data) => {
-      response.json(data[0].product);
-    })
-    .catch((err) => console.log(err));
+
+  sendResponse(response, () => {
+    return database.getProductById(productId, hostname);
+  });
 });
+
 /* Создание категории */
 router.route("/category").post((request, response) => {
   const { title, image } = request.body;
 
-  // запрос к БД
-  const result = database.addCategory(title, slugify(title, slugParams), image);
-
-  result
-    .then((data) => {
-      // если в ответе есть ошибка, меняем статус
-      if (data.error) response.statusCode = 400;
-
-      response.json(data);
-    })
-    .catch((err) => console.log(err));
+  sendResponse(response, () => {
+    return database.addCategory(title, slugify(title, slugParams), image);
+  });
 });
 
 /* Создание продукта */
 router.route("/product").post((request, response) => {
   const { title, image, description, price } = request.body;
 
-  // запрос к БД
-  const result = database.addProduct(
-    title,
-    slugify(title, slugParams),
-    image,
-    description,
-    price
-  );
-
-  result
-    .then((data) => {
-      // если в ответе есть ошибка, меняем статус
-      if (data.error) response.statusCode = 400;
-
-      response.json(data);
-    })
-    .catch((err) => console.log(err));
+  sendResponse(response, () => {
+    return database.addProduct(
+      title,
+      slugify(title, slugParams),
+      image,
+      description,
+      price
+    );
+  });
 });
 
-/* Создание продукта */
-router.route("/users/accessKey").get((request, response) => {
-  // запрос к БД
-  const result = database.addUser();
-
-  result
-    .then((data) => {
-      // если в ответе есть ошибка, меняем статус
-      if (data?.error) response.statusCode = 400;
-
-      response.json(data);
-    })
-    .catch((err) => console.log(err));
+/* Создание пользователя */
+router.route("/users/accessKey").get(async (request, response) => {
+  sendResponse(response, () => {
+    return database.addUser();
+  });
 });
 
-router.route("/baskets").get((request, response) => {
-  const { accessKey } = request.query;
-  const result = database.getCart(accessKey);
-
-  result
-    .then((data) => {
-      // если в ответе есть ошибка, меняем статус
-      if (data?.error) response.statusCode = 400;
-
-      response.json(data);
-    })
-    .catch((err) => console.log(err));
+/* Получение корзины */
+router.route("/baskets").get(async (request, response) => {
+  sendResponse(response, () => {
+    const { accessKey } = request.query;
+    return database.getCart(accessKey);
+  });
 });
 export default router;
