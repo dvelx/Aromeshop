63b97a6ba82d96441407535f120df602c89ff7ff
DmitriyHoff <goffdmitriy@gmail.com>
2023-09-02 12:41:37 +0200

    update /api/baskets/

--- backend/controllers/database.controller.js
+++ backend/controllers/database.controller.js
@@ -19,9 +19,9 @@ function getRequestHostUrl(request) {
   return `${request.protocol}://${request.get("host")}/`;
 }
 
-async function sendResponse(response, callback) {
+async function sendResponse(response, data) {
   try {
-    const data = await callback();
+    // const data = await data();
 
     // если в ответе есть ошибка, меняем статус
     if (data?.error) response.statusCode = 400;
@@ -33,136 +33,151 @@ async function sendResponse(response, callback) {
 }
 
 /* Получение списка категорий */
-router.route("/categories").get((request, response) => {
-  sendResponse(response, () => {
-    return database.getCategories();
-  });
+router.route("/categories").get(async (request, response) => {
+  sendResponse(response, await database.getCategories());
 });
 
 /* Получение списка производителей */
-router.route("/brands").get((request, response) => {
-  sendResponse(response, () => {
-    return database.getBrands();
-  });
+router.route("/brands").get(async (request, response) => {
+  sendResponse(response, await database.getBrands());
 });
 
 /* Получение списка товаров */
-router.route("/products").get((request, response) => {
+router.route("/products").get(async (request, response) => {
   const hostname = getRequestHostUrl(request);
   const { limit, page } = request.query;
-  sendResponse(response, () => {
-    return database.getProducts(hostname, { limit, page });
-  });
+  sendResponse(response, await database.getProducts(hostname, { limit, page }));
 });
 
 /* Получение товара по id */
-router.route("/product/:id").get((request, response) => {
+router.route("/product/:id").get(async (request, response) => {
   const hostname = getRequestHostUrl(request);
   const productId = request.params.id;
 
-  sendResponse(response, () => {
-    return database.getProductById(productId, hostname);
-  });
+  sendResponse(response, await database.getProductById(productId, hostname));
 });
 
 /* Создание категории */
-router.route("/category").post((request, response) => {
+router.route("/category").post(async (request, response) => {
   const { title, image } = request.body;
 
-  sendResponse(response, () => {
-    return database.addCategory(title, slugify(title, slugParams), image);
-  });
+  sendResponse(
+    response,
+    await database.addCategory(title, slugify(title, slugParams), image)
+  );
 });
 
 /* Создание продукта */
-router.route("/product").post((request, response) => {
+router.route("/product").post(async (request, response) => {
   const { title, image, description, price } = request.body;
 
-  sendResponse(response, () => {
-    return database.addProduct(
+  sendResponse(
+    response,
+    await database.addProduct(
       title,
       slugify(title, slugParams),
       image,
       description,
       price
-    );
-  });
+    )
+  );
 });
 
 /* Создание пользователя */
 router.route("/users/accessKey").get(async (request, response) => {
-  sendResponse(response, () => {
-    return database.addUser();
-  });
+  sendResponse(response, await database.addUser());
 });
 
 /* Получение корзины */
 router.route("/baskets").get(async (request, response) => {
   const hostname = getRequestHostUrl(request);
   const { accessKey } = request.query;
-
+  if (!accessKey) {
+    sendResponse(response, { error: "accessKey required" });
+    return;
+  }
   let result = await database.getCart({ accessKey, hostname });
   if (!result) result = { error: "Wrong accessKey" };
 
-  sendResponse(response, () => {
-    return result;
-  });
+  sendResponse(response, result);
 });
 
 /* Добавление в корзину */
-router.route("/baskets").post((request, response) => {
-  sendResponse(response, async () => {
-    const { cartId, productId, quantity } = request.body;
-    const { accessKey } = request.query;
-    const hostname = getRequestHostUrl(request);
+router.route("/baskets").post(async (request, response) => {
+  const { productId, quantity } = request.body;
+  const { accessKey } = request.query;
+  if (!accessKey) {
+    sendResponse(response, { error: "accessKey required" });
+    return;
+  }
+  const hostname = getRequestHostUrl(request);
 
-    let result = await database.getCart({ accessKey, hostname });
-    if (!result) result = { error: "Wrong accessKey" };
-    else {
-      await database.addProductToCart({ cartId, productId, quantity });
-      result = await database.getCart({ accessKey, hostname });
-    }
-
-    return result;
-  });
+  let error = {};
+  let result = {};
+  let cart = await database.getCart({ accessKey, hostname });
+  if (!cart) error = { ...error, accessKey: "Wrong accessKey" };
+  if (!productId) error = { ...error, productId: "productId required" };
+  if (!quantity) error = { ...error, quantity: "quantity undefined" };
+  if (Object.keys(error).length > 0) {
+    sendResponse(response, { error });
+    return;
+  } else {
+    await database.addProductToCart({ cartId: cart.id, productId, quantity });
+    result = await database.getCart({ accessKey, hostname });
+  }
+  sendResponse(response, result);
 });
 
 /* Изменение количества товара в корзине */
-router.route("/baskets").put((request, response) => {
-  const { cartId, productId, quantity } = request.body;
-  if (!cartId)
-    sendResponse(response, () => {
-      return { error: "cartId undefined" };
+router.route("/baskets").put(async (request, response) => {
+  const { productId, quantity } = request.body;
+  const { accessKey } = request.query;
+  if (!accessKey) {
+    sendResponse(response, { error: "accessKey required" });
+    return;
+  }
+  let error = {};
+  let result = {};
+  const hostname = getRequestHostUrl(request);
+  let cart = await database.getCart({ accessKey, hostname });
+  if (!cart) error = { ...error, accessKey: "Wrong accessKey" };
+  if (!productId) error = { ...error, productId: "productId required" };
+  if (!quantity) error = { ...error, quantity: "quantity undefined" };
+  if (Object.keys(error).length > 0) {
+    sendResponse(response, { error });
+    return;
+  } else {
+    await database.setProductQuantity({
+      cartId: cart.id,
+      productId,
+      quantity,
     });
-  else if (!productId)
-    sendResponse(response, () => {
-      return { error: "productId undefined" };
-    });
-  else if (!quantity)
-    sendResponse(response, () => {
-      return { error: "quantity undefined" };
-    });
-  else
-    sendResponse(response, () => {
-      return database.setProductQuantity({ cartId, productId, quantity });
-    });
+    result = await database.getCart({ accessKey, hostname });
+    sendResponse(response, result);
+  }
 });
 
 /* Удаление товара из корзины */
-router.route("/baskets").delete((request, response) => {
-  const { cartId, productId } = request.body;
-  if (!cartId)
-    sendResponse(response, () => {
-      return { error: "cartId undefined" };
-    });
-  else if (!productId)
-    sendResponse(response, () => {
-      return { error: "productId undefined" };
-    });
-  else
-    sendResponse(response, () => {
-      return database.deleteProductFromCart({ cartId, productId });
-    });
+router.route("/baskets").delete(async (request, response) => {
+  const { productId } = request.body;
+  const { accessKey } = request.query;
+  if (!accessKey) {
+    sendResponse(response, { error: "accessKey required" });
+    return;
+  }
+  let error = {};
+  let result = {};
+  const hostname = getRequestHostUrl(request);
+  let cart = await database.getCart({ accessKey, hostname });
+  if (!cart) error = { ...error, accessKey: "Wrong accessKey" };
+  if (!productId) error = { ...error, productId: "productId required" };
+
+  if (Object.keys(error).length > 0) {
+    sendResponse(response, { error });
+    return;
+  } else await database.deleteProductFromCart({ cartId: cart.id, productId });
+  result = await database.getCart({ accessKey, hostname });
+  sendResponse(response, result);
 });
 
 router.route("/orders").post(async (request, response) => {
@@ -186,61 +201,60 @@ router.route("/orders").post(async (request, response) => {
     if (!email) {
       error = { ...error, email: "email required" };
     }
-    if (!comment) {
-      error = { ...error, comment: "comment required" };
-    }
+    // if (!comment) {
+    //   error = { ...error, comment: "comment required" };
+    // }
   }
   if (Object.keys(error).length > 0) {
-    sendResponse(response, () => {
-      return { error };
-    });
+    sendResponse(response, { error });
     return;
   }
   const hostname = getRequestHostUrl(request);
   const cart = await database.getCart({ hostname, accessKey });
-
-  database.db.beginTransaction();
   let result = {};
   let lastOrder;
-  try {
-    await database.makeOrder({ name, address, phone, email, comment });
-    [lastOrder] = await database.getLastInsertedOrder();
 
-    cart.items.forEach(async (item) => {
-      const product = await database.getProductById(item.id);
-      await database.addOrderItem({
-        orderId: lastOrder.id,
-        productId: product.id,
-        productTitle: product.title,
-        quantity: item.quantity,
-        price: product.price,
+  if (cart) {
+    database.db.beginTransaction();
+
+    try {
+      await database.makeOrder({ name, address, phone, email, comment });
+      [lastOrder] = await database.getLastInsertedOrder();
+
+      cart.items.forEach(async (item) => {
+        const product = await database.getProductById(item.id);
+        await database.addOrderItem({
+          orderId: lastOrder.id,
+          productId: product.id,
+          productTitle: product.title,
+          quantity: item.quantity,
+          price: product.price,
+        });
       });
-    });
-    await database.deleteCartById(cart.id);
-    database.db.commit();
-    result = lastOrder;
-  } catch (error) {
-    console.log(error);
-    database.db.rollback();
+      await database.deleteCartById(cart.id);
+      database.db.commit();
+      result = lastOrder;
+    } catch (error) {
+      console.log(error);
+      database.db.rollback();
+    }
+    const items = await database.getOrderItems(lastOrder.id);
+    for (let i = 0; i < items.length; i++) {
+      const product = await database.getProductById(items[i]["product_id"]);
+      items[i] = { ...items[i], product };
+    }
+    result = { ...result, items };
+  } else {
+    result = { error: "Wrong accessKey" };
   }
-  const items = await database.getOrderItems(lastOrder.id);
-  for (let i = 0; i < items.length; i++) {
-    const product = await database.getProductById(items[i]["product_id"]);
-    items[i] = { ...items[i], product };
-  }
-  result = { ...result, items };
-  sendResponse(response, () => {
-    return result;
-  });
+  sendResponse(response, result);
 });
 
 router.route("/order/:id").get(async (request, response) => {
   const orderId = request.params.id;
   const [order] = await database.getOrderById(orderId);
   if (!order) {
-    sendResponse(response, () => {
-      return { error: "Order not found!" };
-    });
+    sendResponse(response, { error: "Order not found!" });
     return;
   }
 
@@ -251,8 +265,6 @@ router.route("/order/:id").get(async (request, response) => {
   }
 
   const result = { ...order, items };
-  sendResponse(response, () => {
-    return result;
-  });
+  sendResponse(response, result);
 });
 export default router;
